8f4903246bc0a25fd92bbf68bf302a61127cb6c6
Andrey Semenov <macher-bobru@yandex.ru>
2025-04-13 15:41:18 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/travel/localers/LocalersApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.travel.localers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/yandex/travel/localers/LocalersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yandex.travel.localers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
